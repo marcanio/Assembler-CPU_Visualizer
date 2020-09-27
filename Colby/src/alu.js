6f2653484b35cfb9fd34bac080d9b61ab6bcd6f0
Colby McKinley <colby@pop-os.localdomain>
2020-09-27 18:17:35 -0500

    fixing html serving issues.  However it wont make js calls

---
+++ Colby/src/alu.js
@@ -0,0 +1,42 @@
+/**
+ * This funtion simulates the alu.
+ * @param operand_a: integer operand
+ * @param operand_b: integer operand
+ * @param opcode: string opcode
+ * @param flags: array of status bits
+ * @since 1.0
+ * @returns the resulting computation
+ * @author Colby McKinley
+ * 
+*/
+
+export function alu(operand_a, operand_b, opcode, flags)
+{
+    let result = 0;
+    switch(opcode)
+    {
+        case "ADD":
+            result = operand_a + operand_b;            
+            break;
+        case "SUB":
+            result = operand_a + operand_b;
+            break;
+        case "SLT":
+            result = operand_a < operand_b;
+            break;
+        case "OR":
+            result = operand_a | operand_b;
+            break;
+        case "AND":
+            result = operand_a & operand_b;
+            break;
+        case "NOR":
+            result = ~(operand_a & operand_b);
+            break;
+        default:
+            break;
+    }
+    flags[0] = result === 0;
+    return result;
+};
+
--- Colby/src/index.js
+++ Colby/src/index.js
@@ -1,39 +1,102 @@
-console.log("Hello World");
+//import {alu} from "./alu.js"
+import * as http from 'http'; //ES 6
+import fs from 'fs';
+import * as path from 'path';
 
-function alu(operand_a, operand_b, opcode)
-{
-    let result = 0;
-    switch(opcode)
-    {
-        case "ADD":
-            result = operand_a + operand_b;
-            break;
-        case "SUB":
-            result = operand_a + operand_b;
-            break;
-        case "SLT":
-            result = operand_a < operand_b;
-            break;
-        case "OR":
-            result = operand_a | operand_b;
-            break;
-        case "AND":
-            result = operand_a & operand_b;
-            break;
-        case "NOR":
-            result = ~(operand_a & operand_b);
-            break;
-        default:
-            break;
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = './src' + request.url;
+    if (filePath == './src/') {
+        filePath = './src/index.html';
     }
-    return result;
-};
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
 
-function compute() {
-    const inst = document.getElementById("instruction").value;
-    console.log(inst);
-    let parse_val = inst.split(' ');
-    let result = alu(parseInt(parse_val[1]),parseInt(parse_val[2]),parse_val[0]);
-    console.log(result);
-    document.getElementById("result").innerText = result +"";
-};
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+    // // if(req.method == "GET" && req.url == '/'){
+       
+    // // res.writeHead(200, {"Content-Type":"text/html"});
+    // //     fs.createReadStream("src/index.html").pipe(res);
+
+       
+    // // }
+    // // else if(req.url.match("\.css$")){
+    // //     var cssPath = path.join(__dirname, 'src', req.url);
+    // //     var fileStream = fs.createReadStream(cssPath, "UTF-8");
+    // //     res.writeHead(200, {"Content-Type": "text/css"});
+    // //     fileStream.pipe(res);
+
+    // // }
+    // if(req.url === "/"){
+    //     fs.readFile("./src/index.html", "UTF-8", function(err, html){
+    //         res.writeHead(200, {"Content-Type": "text/html"});
+    //         res.end(html);
+    //     });
+    // }else if(req.url.match("\.css$")){
+    //     var cssPath = path.join(req.url);
+    //     var fileStream = fs.createReadStream(cssPath, "UTF-8");
+    //     res.writeHead(200, {"Content-Type": "text/css"});
+    //     fileStream.pipe(res);
+
+    // }/*else if(req.url.match("\.png$")){
+    //     var imagePath = path.join(__dirname, 'src', req.url);
+    //     var fileStream = fs.createReadStream(imagePath);
+    //     res.writeHead(200, {"Content-Type": "image/png"});
+    //     fileStream.pipe(res);
+    // }*/else{
+    //     res.writeHead(404, {"Content-Type": "text/html"});
+    //     res.end("No Page Found");
+    // }
+}).listen(3000, "127.0.0.1");
+
+function sendFileContent(response, fileName, contentType){
+	fs.readFile(fileName, function(err, data){
+		if(err){
+            console.log(err);
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type': contentType});
+			response.write(data);
+		}
+		response.end();
+	});
+}
+
