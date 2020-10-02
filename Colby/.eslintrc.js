9d2e5d6f591521771b79c6de3301559d5bc3a44e
Colby McKinley <colby@pop-os.localdomain>
2020-10-02 16:48:33 -0500

    installing eslint into project

---
+++ Colby/.eslintrc.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node":true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 12,
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
--- Colby/src/simulator.js
+++ Colby/src/simulator.js
@@ -1,4 +1,4 @@
-import {alu} from "./alu.js"
+import {alu} from "./alu.js";
 // import $ from '/js/libs/jquery/dist/jquery.js'
 // $("btn").on(
 //     "click" ,function(){
@@ -20,12 +20,12 @@ import {alu} from "./alu.js"
 
 
 export function compute() {
-    const inst = document.getElementById("instruction").value;
-    console.log(inst);
-    let parse_val = inst.split(' ');
-    let result =  alu(parseInt(parse_val[1]),parseInt(parse_val[2]),parse_val[0]);
-    console.log(result);
-    document.getElementById("result").innerText = result +"";
-};
+	const inst = document.getElementById("instruction").value;
+	console.log(inst);
+	let parse_val = inst.split(" ");
+	let result =  alu(parseInt(parse_val[1]),parseInt(parse_val[2]),parse_val[0]);
+	console.log(result);
+	document.getElementById("result").innerText = result +"";
+}
 
 window.compute = compute;
