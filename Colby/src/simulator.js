c7fcdf0477cc5e0283290d30f78690f5dfc24fd9
Colby McKinley <colby@pop-os.localdomain>
2020-10-14 17:32:24 -0500

    more updates

--- Colby/src/simulator.js
+++ Colby/src/simulator.js
@@ -92,30 +92,23 @@ export function compute() {
 
 window.addEventListener("load", function() {
 	init();
+	const CODE_MEM_WIDTH = 350;
+	const CODE_MEM_HEIGHT = 450;
+
+	const REGISTER_FILE_WIDTH = 350;
+	const REGISTER_FILE_HEIGHT = 500;
+
 	const MUX_POLYGON = [0,0, 50,30, 50,170, 0,200];
 	const ALU_POLYGON = [0,0, 100,75, 100,225, 0,300, 0,200, 25,150, 0,100 ];
 	const FLAGS_POLYGON = [0,0, 150,0, 150,100, 0,100];
-	const CODE_MEM_POLYGON = [0,0, 350,0, 350,450, 0,450];
+	const CODE_MEM_POLYGON = [0,0, CODE_MEM_WIDTH,0, CODE_MEM_WIDTH,CODE_MEM_HEIGHT, 0,CODE_MEM_HEIGHT];
 	const DATA_MEM_POLYGON = [0,0, 350,0, 350,450, 0,450];
 	const OPCODE_DECODER_POLYGON = [0,0, 200,0, 200,100, 0,100];
 	const CONTROL_POLYGON = [0,0, 550,0, 550,100, 0,100];
-	const REGISTER_FILE_POLYGON = [0,0, 350,0, 350,300, 0,300];
+	const REGISTER_FILE_POLYGON = [0,0, REGISTER_FILE_WIDTH,0, REGISTER_FILE_WIDTH,REGISTER_FILE_HEIGHT, 0,REGISTER_FILE_HEIGHT];
 	const PC_VALUE_POLYGON = [0,0, 200,0, 200,150, 0,150];
 	const PC_UPDATE_POLYGON = [0,0, 200,0, 200,150, 0,150];
 	const ARROW = [5,0, 10,0, 10, 40, 15, 40, 7.5,50, 0,40, 5,40];
-
-	const MUX_ALU_WIRE = [0, 0, 50, 0];
-	const ALU_RESULT_WIRE = [0, 0, 150, 0];
-	const IMEM_DECODER_WIRE = [0, 0, 125, 0];
-	const OPCODE_DECODER_CONTROL_WIRE = [0, 0, 200, 0];
-	const ALU_FLAGS_WIRE = [0, 0, 200, -25];
-	const READ_A_WIRE = [0, 0, 150, 0];
-	const READ_B_WIRE = [0, 0, 50, 0];
-	const PC_UPDATE_MUX_WIRE = [0, 0, 100, 0];
-	const MUX_PC_VALUE_WIRE = [0, 0, 150, 0];
-
-
-
 
 	const MUX_FALSE = [20, 60];
 	const MUX_TRUE = [20, 140];
@@ -124,34 +117,61 @@ window.addEventListener("load", function() {
 
 	const MUX0_OFFSET = [900 , 650];
 	const MUX1_OFFSET = [1250, 600];
-	const MUX2_OFFSET = [1250, 850];
+	const MUX2_OFFSET = [1250, 950];
 	const MUX3_OFFSET = [2000, 800];
-	const MUX4_OFFSET = [500 , 1050];
+	const MUX4_OFFSET = [500 , 1150];
 
 	const ALU_OFFSET = [1000, 500];
 	const FLAGS_OFFSET = [1300, 500];
-	const CODE_MEM_OFFSET = [25, 25];
-	const OPCODE_DECODER_OFFSET = [500, 25];
-	const CONTROL_OFFSET = [900, 25];
+	const CODE_MEM_OFFSET = [100, 100];
+	const OPCODE_DECODER_OFFSET = [500, 100];
+	const CONTROL_OFFSET = [900, 100];
 	const REGISTER_FILE_OFFSET = [500, 500];
-	const PC_VALUE_OFFSET = [700, 1050];
-	const PC_UPDATE_OFFSET = [200, 1050];
+	const PC_VALUE_OFFSET = [700, 1150];
+	const PC_UPDATE_OFFSET = [200, 1150];
 	const DATA_MEM_OFFSET = [1400, 900];
 
-	const MUX_ALU_WIRE_OFFSET = [950, 750];
-	const ALU_RESULT_WIRE_OFFSET = [1100, 650];
-	const IMEM_DECODER_WIRE_OFFSET = [375, 50];
-	const OPCODE_DECODER_CONTROL_WIRE_OFFSET = [700, 50];
-	const ALU_FLAGS_WIRE_OFFSET = [1100, 600];
-	const READ_A_WIRE_OFFSET = [850, 550];
-	const READ_B_WIRE_OFFSET = [850, 700];
-	const PC_UPDATE_MUX_FALSE_WIRE_OFFSET = [400, 1100];
-	const PC_UPDATE_MUX_TRUE_WIRE_OFFSET = [400, 1175];
-	const MUX_PC_VALUE_WIRE_OFFSET = [550, 1150];
+	const MUX_ALU_WIRE = [0, 0, ALU_OFFSET[0] - (MUX0_OFFSET[0] + MUX_POLYGON[2]), 0];
+	const ALU_RESULT_WIRE = [0, 0, MUX1_OFFSET[0] - (ALU_OFFSET[0] + ALU_POLYGON[2]), 0];
+	const IMEM_DECODER_WIRE = [0, 0, OPCODE_DECODER_OFFSET[0] - (CODE_MEM_OFFSET[0] + CODE_MEM_WIDTH), 0];
+	const OPCODE_DECODER_CONTROL_WIRE = [0, 0, CONTROL_OFFSET[0] - (OPCODE_DECODER_OFFSET[0] + OPCODE_DECODER_POLYGON[2]), 0];
+	const ALU_FLAGS_WIRE = [0, 0, 200, -25];
+	const READ_A_WIRE = [0, 0, ALU_OFFSET[0] - (REGISTER_FILE_OFFSET[0] + REGISTER_FILE_WIDTH), 0];
+	const READ_B_WIRE = [0, 0, MUX0_OFFSET[0] - (REGISTER_FILE_OFFSET[0] + REGISTER_FILE_WIDTH), 0];
+	const PC_UPDATE_MUX_WIRE = [0, 0, MUX4_OFFSET[0] - (PC_UPDATE_OFFSET[0] + PC_UPDATE_POLYGON[2]), 0];
+	const MUX_PC_VALUE_WIRE = [0, 0, PC_VALUE_OFFSET[0] - (MUX4_OFFSET[0] + MUX_POLYGON[2]), 0];
 
+	const MUX_ALU_WIRE_OFFSET = [MUX0_OFFSET[0] + MUX_POLYGON[2], MUX0_OFFSET[1] + 100];
+	const ALU_RESULT_WIRE_OFFSET = [ALU_OFFSET[0] + ALU_POLYGON[2], ALU_OFFSET[1] + 150];
+	const IMEM_DECODER_WIRE_OFFSET = [CODE_MEM_OFFSET[0] + CODE_MEM_WIDTH, CODE_MEM_OFFSET[1] + 25];
+	const OPCODE_DECODER_CONTROL_WIRE_OFFSET = [OPCODE_DECODER_OFFSET[0] + OPCODE_DECODER_POLYGON[2], OPCODE_DECODER_OFFSET[1] + 25];
+	const ALU_FLAGS_WIRE_OFFSET = [ALU_OFFSET[0] + ALU_POLYGON[2], ALU_OFFSET[1] + 100];
+	const READ_A_WIRE_OFFSET = [REGISTER_FILE_WIDTH + REGISTER_FILE_OFFSET[0], REGISTER_FILE_OFFSET[1] + 50];
+	const READ_B_WIRE_OFFSET = [REGISTER_FILE_WIDTH + REGISTER_FILE_OFFSET[0], REGISTER_FILE_OFFSET[1] + 200];
+	const PC_UPDATE_MUX_FALSE_WIRE_OFFSET = [PC_UPDATE_OFFSET[0] + PC_UPDATE_POLYGON[2], PC_UPDATE_OFFSET[1] + 50];
+	const PC_UPDATE_MUX_TRUE_WIRE_OFFSET = [PC_UPDATE_OFFSET[0] + PC_UPDATE_POLYGON[2], PC_UPDATE_OFFSET[1] + 125];
+	const MUX_PC_VALUE_WIRE_OFFSET = [MUX4_OFFSET[0] + MUX_POLYGON[2], MUX4_OFFSET[1] + 100];
 
+	const IMEM_C1_OFFSET = [CODE_MEM_WIDTH/2 + CODE_MEM_OFFSET[0], CODE_MEM_OFFSET[0] - ARROW[9]];
+	const MUX_C2_OFFSET = [MUX4_OFFSET[0] + 20, MUX4_OFFSET[1] + (15 - ARROW[9])];
+	const PC_VALUE_C3_OFFSET = [PC_VALUE_POLYGON[2]/2 + PC_VALUE_OFFSET[0], PC_VALUE_OFFSET[1] - ARROW[9]];
+	const READ_A_C4_OFFSET = [REGISTER_FILE_OFFSET[0] + REGISTER_FILE_WIDTH/2 + 1 * 25, REGISTER_FILE_OFFSET[1] + (100-ARROW[9])];
+	const READ_A_C5_OFFSET = [REGISTER_FILE_OFFSET[0] + REGISTER_FILE_WIDTH/2 + 2 * 25, REGISTER_FILE_OFFSET[1] + (100-ARROW[9])];
+	const READ_B_C6_OFFSET = [REGISTER_FILE_OFFSET[0] + REGISTER_FILE_WIDTH/2 + 1 * 25, REGISTER_FILE_OFFSET[1] + (200-ARROW[9])];
+	const READ_B_C7_OFFSET = [REGISTER_FILE_OFFSET[0] + REGISTER_FILE_WIDTH/2 + 2 * 25, REGISTER_FILE_OFFSET[1] + (200-ARROW[9])];
+	const REG_FILE_C8_OFFSET = [REGISTER_FILE_OFFSET[0] + 1 * 30, REGISTER_FILE_OFFSET[1] - ARROW[9]];
+	const REG_FILE_C9_OFFSET = [REGISTER_FILE_OFFSET[0] + 2 * 30, REGISTER_FILE_OFFSET[1] - ARROW[9]];
+	const REG_FILE_C10_OFFSET = [REGISTER_FILE_OFFSET[0] + 3 * 30, REGISTER_FILE_OFFSET[1] - ARROW[9]];
+	const MUX_C11_OFFSET = [MUX0_OFFSET[0] + 20, MUX0_OFFSET[1] + (15 - ARROW[9])];
+	const ALU_C12_OFFSET = [ALU_OFFSET[0] + 1 * 25, ALU_OFFSET[1] - ARROW[9]];
+	const ALU_C13_OFFSET = [ALU_OFFSET[0] + 2 * 25, ALU_OFFSET[1] - ARROW[9]];
+	const FLAGS_C14_OFFSET = [FLAGS_POLYGON[2]/2 + FLAGS_OFFSET[0], FLAGS_OFFSET[1] - ARROW[9]];
+	const MUX_C15_OFFSET = [MUX1_OFFSET[0] + 20, MUX1_OFFSET[1] + (15 - ARROW[9])];
+	const MUX_C16_OFFSET = [MUX2_OFFSET[0] + 20, MUX2_OFFSET[1] + (15 - ARROW[9])];
+	const DMEM_C17_OFFSET = [DATA_MEM_POLYGON[2]/2 + DATA_MEM_OFFSET[0], DATA_MEM_OFFSET[1] - ARROW[9]];
+	const MUX_C18_OFFSET = [MUX3_OFFSET[0] + 20, MUX3_OFFSET[1] + (15 - ARROW[9])];
 
-	const CONTROL_ARROW_OFFSET = [900, 125];
+	const CONTROL_ARROW_OFFSET = [CONTROL_OFFSET[0], CONTROL_OFFSET[1] + CONTROL_POLYGON[7]];
 
 
 	const ARROW_DIST_BETWEEN = 28;
@@ -380,7 +400,94 @@ window.addEventListener("load", function() {
 	var mux_c2 = new Polygon([...ARROW]);
 	mux_c2.attribute(STYLE_ATTR, ARROW_STYLE);
 	mux_c2.attribute(ID_ATTR, "mux_c2");
-	mux_c2.translate(MUX0_OFFSET[0] + 25, MUX0_OFFSET[1] - 40);
+	mux_c2.translate(MUX_C2_OFFSET[0], MUX_C2_OFFSET[1] );
+
+	var mux_c11 = new Polygon([...ARROW]);
+	mux_c11.attribute(STYLE_ATTR, ARROW_STYLE);
+	mux_c11.attribute(ID_ATTR, "mux_c11");
+	mux_c11.translate(MUX_C11_OFFSET[0], MUX_C11_OFFSET[1] );
+
+	var mux_c15 = new Polygon([...ARROW]);
+	mux_c15.attribute(STYLE_ATTR, ARROW_STYLE);
+	mux_c15.attribute(ID_ATTR, "mux_c15");
+	mux_c15.translate(MUX_C15_OFFSET[0], MUX_C15_OFFSET[1] );
+
+	var mux_c16 = new Polygon([...ARROW]);
+	mux_c16.attribute(STYLE_ATTR, ARROW_STYLE);
+	mux_c16.attribute(ID_ATTR, "mux_c16");
+	mux_c16.translate(MUX_C16_OFFSET[0], MUX_C16_OFFSET[1] );
+
+	var mux_c18 = new Polygon([...ARROW]);
+	mux_c18.attribute(STYLE_ATTR, ARROW_STYLE);
+	mux_c18.attribute(ID_ATTR, "mux_c18");
+	mux_c18.translate(MUX_C18_OFFSET[0], MUX_C18_OFFSET[1] );
+
+	var pc_value_c3 = new Polygon([...ARROW]);
+	pc_value_c3.attribute(STYLE_ATTR, ARROW_STYLE);
+	pc_value_c3.attribute(ID_ATTR, "pc_value_c3");
+	pc_value_c3.translate(PC_VALUE_C3_OFFSET[0], PC_VALUE_C3_OFFSET[1] );
+
+	var imem_c1 = new Polygon([...ARROW]);
+	imem_c1.attribute(STYLE_ATTR, ARROW_STYLE);
+	imem_c1.attribute(ID_ATTR, "imem_c1");
+	imem_c1.translate(IMEM_C1_OFFSET[0], IMEM_C1_OFFSET[1]);
+	
+	var read_a_c4 = new Polygon([...ARROW]);
+	read_a_c4.attribute(STYLE_ATTR, ARROW_STYLE);
+	read_a_c4.attribute(ID_ATTR, "read_a_c4");
+	read_a_c4.translate(READ_A_C4_OFFSET[0], READ_A_C4_OFFSET[1]);
+
+	var read_a_c5 = new Polygon([...ARROW]);
+	read_a_c5.attribute(STYLE_ATTR, ARROW_STYLE);
+	read_a_c5.attribute(ID_ATTR, "read_a_c5");
+	read_a_c5.translate(READ_A_C5_OFFSET[0], READ_A_C5_OFFSET[1]);
+
+	var read_b_c6 = new Polygon([...ARROW]);
+	read_b_c6.attribute(STYLE_ATTR, ARROW_STYLE);
+	read_b_c6.attribute(ID_ATTR, "read_b_c6");
+	read_b_c6.translate(READ_B_C6_OFFSET[0], READ_B_C6_OFFSET[1]);
+
+	var read_b_c7 = new Polygon([...ARROW]);
+	read_b_c7.attribute(STYLE_ATTR, ARROW_STYLE);
+	read_b_c7.attribute(ID_ATTR, "read_b_c7");
+	read_b_c7.translate(READ_B_C7_OFFSET[0], READ_B_C7_OFFSET[1]);
+
+	var reg_file_c8 = new Polygon([...ARROW]);
+	reg_file_c8.attribute(STYLE_ATTR, ARROW_STYLE);
+	reg_file_c8.attribute(ID_ATTR, "reg_file_c8");
+	reg_file_c8.translate(REG_FILE_C8_OFFSET[0], REG_FILE_C8_OFFSET[1]);
+
+	var reg_file_c9 = new Polygon([...ARROW]);
+	reg_file_c9.attribute(STYLE_ATTR, ARROW_STYLE);
+	reg_file_c9.attribute(ID_ATTR, "reg_file_c9");
+	reg_file_c9.translate(REG_FILE_C9_OFFSET[0], REG_FILE_C9_OFFSET[1]);
+
+	var reg_file_c10 = new Polygon([...ARROW]);
+	reg_file_c10.attribute(STYLE_ATTR, ARROW_STYLE);
+	reg_file_c10.attribute(ID_ATTR, "reg_file_c10");
+	reg_file_c10.translate(REG_FILE_C10_OFFSET[0], REG_FILE_C10_OFFSET[1]);
+
+	var alu_c12 = new Polygon([...ARROW]);
+	alu_c12.attribute(STYLE_ATTR, ARROW_STYLE);
+	alu_c12.attribute(ID_ATTR, "alu_c12");
+	alu_c12.translate(ALU_C12_OFFSET[0], ALU_C12_OFFSET[1]);
+
+	var alu_c13 = new Polygon([...ARROW]);
+	alu_c13.attribute(STYLE_ATTR, ARROW_STYLE);
+	alu_c13.attribute(ID_ATTR, "alu_c13");
+	alu_c13.translate(ALU_C13_OFFSET[0], ALU_C13_OFFSET[1]);
+
+	var flags_c14 = new Polygon([...ARROW]);
+	flags_c14.attribute(STYLE_ATTR, ARROW_STYLE);
+	flags_c14.attribute(ID_ATTR, "flags_c14");
+	flags_c14.translate(FLAGS_C14_OFFSET[0], FLAGS_C14_OFFSET[1]);
+
+	var dmem_c17 = new Polygon([...ARROW]);
+	dmem_c17.attribute(STYLE_ATTR, ARROW_STYLE);
+	dmem_c17.attribute(ID_ATTR, "dmem_c17");
+	dmem_c17.translate(DMEM_C17_OFFSET[0], DMEM_C17_OFFSET[1]);
+
+
 
 	var control_c1 = new Polygon([...ARROW]);
 	control_c1.attribute(STYLE_ATTR, ARROW_STYLE);
@@ -514,6 +621,22 @@ window.addEventListener("load", function() {
 	svg.appendChild(pc_update_true_wire.node);
 	svg.appendChild(mux_pc_val_wire.node);
 	svg.appendChild(mux_c2.node);
+	svg.appendChild(read_a_c4.node);	
+	svg.appendChild(read_a_c5.node);	
+	svg.appendChild(read_b_c6.node);	
+	svg.appendChild(read_b_c7.node);	
+	svg.appendChild(mux_c11.node);	
+	svg.appendChild(mux_c15.node);	
+	svg.appendChild(mux_c16.node);
+	svg.appendChild(mux_c18.node);
+	svg.appendChild(reg_file_c8.node);	
+	svg.appendChild(reg_file_c9.node);	
+	svg.appendChild(reg_file_c10.node);
+	svg.appendChild(alu_c12.node);
+	svg.appendChild(alu_c13.node);
+	svg.appendChild(flags_c14.node);
+	svg.appendChild(dmem_c17.node);
+	svg.appendChild(pc_value_c3.node);
 	svg.appendChild(control_c1.node);
 	svg.appendChild(control_c2.node);
 	svg.appendChild(control_c3.node);
@@ -532,7 +655,7 @@ window.addEventListener("load", function() {
 	svg.appendChild(control_c16.node);
 	svg.appendChild(control_c17.node);
 	svg.appendChild(control_c18.node);
-
+	svg.appendChild(imem_c1.node);
 
 
 
