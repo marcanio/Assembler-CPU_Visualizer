90451937bcaccf2de2d7c0c0c5076001e641e49b
Brady Kolosik <bkolosik@iastate.edu>
2021-02-13 18:30:06 -0600

    preparing for merge

--- i281/src/index.js
+++ i281/src/index.js
@@ -49,4 +49,4 @@ http.createServer(function (request, response) {
 			response.end(content, "utf-8");
 		}
 	});
-}).listen(80, "192.168.1.14");
+}).listen(80, "127.0.0.1");
--- i281/src/instructions.js
+++ i281/src/instructions.js
@@ -1,8 +1,8 @@
-export const i0 = "00110100|00000000"
-export const i1 = "00110000|00000001"
-export const i2 = "10001100|00000000"
-export const i3 = "11010011|00000000"
-export const i4 = "11110010|00000011"
-export const i5 = "01000100|00000000"
+export const i0 = "10000000|00000000"
+export const i1 = "10000100|00000001"
+export const i2 = "11010001|00000000"
+export const i3 = "11110001|00000001"
+export const i4 = "10100000|00000010"
+export const i5 = "00000000|00000000"
 
 export const instructions = [i0, i1, i2, i3, i4, i5]
